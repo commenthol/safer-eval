1c29f6a6e304fb650c05056e217e457a0d2cc3c5
commenthol <commenthol@gmail.com>
2019-03-09 21:54:53 +0100

    fix: disallow usage of Object.constructor

--- src/index.js
+++ src/index.js
@@ -40,8 +40,11 @@ class SaferEval {
     if (typeof code !== 'string') {
       throw new TypeError('not a string')
     }
+    let src = 'Object.constructor = function () {};\n'
+    src += 'return ' + code + ';\n'
+
     return vm.runInContext(
-      '(function () {"use strict"; return ' + code + '})()',
+      '(function () {"use strict"; ' + src + '})()',
       this._context,
       this._options
     )
